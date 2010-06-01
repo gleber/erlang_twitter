de8cb216c22b75e402a1140dcd735d9a1ed6bdd9
David N. Welton <davidw@dedasys.com>
2010-06-01 17:23:20 +0200

    Remove debug message.

--- src/twitter_stream_sup.erl
+++ src/twitter_stream_sup.erl
@@ -9,7 +9,6 @@
 %% returns the Spec for the supervisor (called by the
 %% supervisor:start_link above)
 init([Url, Callback]) ->
-    io:format("RECVD ~p ~p~n", [Url, Callback]),
     Spec = [{twitter_stream,
 	     {twitter_stream, start_link, [Url, Callback]}, % this gets run via apply(M, F, A)
 	     transient, 2000, worker, [twitter_stream]}],
