11ac81494d88fe4bd4c13f2583a70a02364ddd24
David N. Welton <davidw@dedasys.com>
2010-06-14 15:24:47 +0200

    Added status_to_json and user_to_json functions.
    Note that they don't actually return json - they produce output that
    it suitable for mochijson2:encode.

--- src/twitter_client_utils.erl
+++ src/twitter_client_utils.erl
@@ -1,6 +1,8 @@
 -module(twitter_client_utils).
 
--export([url_encode/1, string_to_int/1, compose_body/1]).
+-export([url_encode/1, string_to_int/1, compose_body/1, status_to_json/1, user_to_json/1]).
+
+-include("twitter_client.hrl").
 
 %% some utility functions stolen from yaws_api module
 
@@ -63,3 +65,49 @@ atol(Str) when is_atom(Str) ->
     atom_to_list(Str);
 atol(Str) ->
     Str.
+
+%% Takes a status record and returns a tuple suitable for handing off
+%% to mochijson2:encode
+
+status_to_json(Status) ->
+    {struct,
+     [
+      {created_at, Status#status.created_at},
+      {id, Status#status.id},
+      {text, Status#status.text},
+      {source, Status#status.source},
+      {truncated, Status#status.truncated},
+      {in_reply_to_status_id, Status#status.in_reply_to_status_id},
+      {in_reply_to_user_id, Status#status.in_reply_to_user_id},
+      {favorited, Status#status.favorited},
+      {user, user_to_json(Status#status.user)}]}.
+
+%% Takes a user record and returns a tuple suitable for handing off to
+%% mochijson2:encode
+
+user_to_json(User) ->
+    {struct,
+     [
+      {id, User#user.id},
+      {name, User#user.name},
+      {screen_name, User#user.screen_name},
+      {location, User#user.location},
+      {description, User#user.description},
+      {profile_image_url, User#user.profile_image_url},
+      {url, User#user.url},
+      {protected, User#user.protected},
+      {followers_count, User#user.followers_count},
+      {status, User#user.status},
+      {profile_background_color, User#user.profile_background_color},
+      {profile_text_color, User#user.profile_text_color},
+      {profile_link_color, User#user.profile_link_color},
+      {profile_sidebar_fill_color, User#user.profile_sidebar_fill_color},
+      {profile_sidebar_border_color, User#user.profile_sidebar_border_color},
+      {friends_count, User#user.friends_count},
+      {created_at, User#user.created_at},
+      {favourites_count, User#user.favourites_count},
+      {utc_offset, User#user.utc_offset},
+      {time_zone, User#user.time_zone},
+      {following, User#user.following},
+      {notifications, User#user.notifications},
+      {statuses_count, User#user.statuses_count}]}.
